3c5e7eef005262dd30f332db7c3af9fba9adf857
MauricioPerezCaram <perezcarammauricio@gmail.com>
2023-08-13 15:20:20 -0300

    cambios finales 2

--- index.js
+++ index.js
@@ -1,133 +1,36 @@
-// // saludo bienvenida al usuario
-// function saludar() {
-//     let nombreUsuario = prompt("Tienda Ropa Flama \nIngresa tu nombre: ");
-//     alert("Hola " + nombreUsuario + "! Bienvenido a nuestra tienda online.\nCon esta aplicación vas a poder ver, agregar , eliminar y hasta modificar las prendas.");
-//   }
-// saludar();
+// saludo bienvenida al usuario
+function saludar() {
+    let nombreUsuario = prompt("Tienda Ropa Flama \nIngresa tu nombre: ");
+    alert("Hola " + nombreUsuario + "! Bienvenido a nuestra tienda online.\nCon esta aplicación vas a poder ver, agregar , eliminar y hasta modificar las prendas.");
+  }
+saludar();
   
-// // ingreso con usuario y contraseña
-// let intentos = 0;
-// let usuario = prompt("Ingresa tu nombre de usuario administrador (Admin)").toLowerCase();
-// let contrasena = prompt("Ingresa la contraseña de administrador (admin123)").toLowerCase();
-// function pedirUsuario(){
-// usuario = prompt("Ingresa un nombre de usuario administrador (Admin)").toLowerCase();
-// }
+// ingreso con usuario y contraseña
+let intentos = 0;
+let usuario = prompt("Ingresa tu nombre de usuario administrador (Admin)").toLowerCase();
+let contrasena = prompt("Ingresa la contraseña de administrador (admin123)").toLowerCase();
+function pedirUsuario(){
+usuario = prompt("Ingresa un nombre de usuario administrador (Admin)").toLowerCase();
+}
 
-// function pedirContrasena() {
-//     contrasena = prompt("Ingresa una contraseña de administrador (admin123)").toLowerCase();
-// }
+function pedirContrasena() {
+    contrasena = prompt("Ingresa una contraseña de administrador (admin123)").toLowerCase();
+}
   
-// while ((usuario !== "admin".toLowerCase() || contrasena !== "admin123".toLowerCase()) && intentos < 2) {
-//     intentos++;
-//     alert("El usuario o contraseña son incorrectos. Te quedan " + (3-intentos) + " intentos.");
-//     pedirUsuario();
-//     pedirContrasena();
-// }
+while ((usuario !== "admin".toLowerCase() || contrasena !== "admin123".toLowerCase()) && intentos < 2) {
+    intentos++;
+    alert("El usuario o contraseña son incorrectos. Te quedan " + (3-intentos) + " intentos.");
+    pedirUsuario();
+    pedirContrasena();
+}
   
-// if (intentos === 2 && (usuario !== "admin".toLowerCase() || contrasena !== "admin123".toLowerCase())) {
-//     alert("Intentaste muchas veces. Acceso denegado.");
-//     console.log("Recarga la página");
-//     throw new Error("Acceso denegado");
-// } else {
-//     alert("Bienvenido al programa.");
-// }
-
-
-
-
-
-
-// const mostrarMercaderia = (prendas) => {
-//     console.clear();
-//     console.log("Prendas disponibles");
-//     prendas.sort((a, b) => a.nombre.localeCompare(b.nombre)); // Ordenamos alfabéticamente
-//     prendas.forEach(prenda => console.log(prenda));
-// }
-
-// mostrarMercaderia(prendas);
-
-// const agregarPrenda = () => {
-//     let nombre = prompt("Ingrese la prenda a agregar");
-//     let precio = parseInt(prompt("Ingrese el precio de la prenda"));
-//     let stock = parseInt(prompt("Ingrese el stock"));
-
-//     let prendaAgregada = new Mercaderia(nombre, precio, stock);
-
-//     prendas.push(prendaAgregada);
-
-//     mostrarMercaderia(prendas);
-// };
-
-
-
-//     const prendaBuscada = prendaExiste("Ingrese el nombre de la prenda a editar");
-
-//     if (!prendaBuscada) return;
-
-//     alert("Menú editar prenda:\n1 - Editar nombre\n2 - Editar precio\n3 - Editar stock");
-
-//     let opcion = parseInt(prompt("Ingrese una opción para editar"));
-
-//     switch (opcion) {
-//         case 1:
-//             let nombre = prompt("Ingrese el nuevo nombre de la prenda");
-//             prendaBuscada.cambiarNombre(nombre);
-//             break;
-//         case 2:
-//             let precio = parseInt(prompt("Ingrese el nuevo precio de la prenda"));
-//             prendaBuscada.cambiarPrecio(precio);
-//             break;
-//         case 3:
-//             let stock = parseInt(prompt("Ingrese el nuevo stock de la prenda"));
-//             prendaBuscada.cambiarStock(stock);
-//             break;
-//         default:
-//             alert("Ingrese una opción correcta");
-//     }
-
-//     mostrarMercaderia(prendas);
-// }
-
-// const prendaExiste = (mensaje) => {
-//     let nombrePrenda = prompt(mensaje);
-
-//     let indice = prendas.findIndex(
-//         (prenda) => prenda.nombre.toLowerCase() === nombrePrenda.toLowerCase()
-//     );
-
-//     if (indice === -1) {
-//         alert(`La prenda ${nombrePrenda} no existe`);
-//         return null;
-//     }
-
-//     return prendas[indice];
-// };
-
-// let encendido = true;
-
-// while (encendido) {
-//     alert("Menú principal:\n1 - Agregar una prenda\n2 - Eliminar una prenda\n3 - Modificar prenda\n4 - Apagar");
-//     let opcion = parseInt(prompt("Ingrese una opción"));
-
-//     switch (opcion) {
-//         case 1:
-//             agregarPrenda();
-//             break;
-//         case 2:
-//             eliminarPrenda();
-//             break;
-//         case 3:
-//             editarPrenda();
-//             break;
-//         case 4:
-//             encendido = false;
-//             break;
-//         default:
-//             alert("Inserte una opción correcta");
-//     }
-// }
-
-// alert("Gracias vuelva pronto");
+if (intentos === 2 && (usuario !== "admin".toLowerCase() || contrasena !== "admin123".toLowerCase())) {
+    alert("Intentaste muchas veces. Acceso denegado.");
+    console.log("Recarga la página");
+    throw new Error("Acceso denegado");
+} else {
+    alert("Bienvenido al programa.");
+}
 
 
 let form = document.querySelector("#form");
@@ -138,7 +41,7 @@ let agregar = document.querySelector("#btn");
 let buscar = document.querySelector("#buscar");
 let contenedorPrendas = document.querySelector("#contenedorPrendas")
 
-// // Clase constructora de mercadería
+// Clase constructora de mercadería
 class Mercaderia {
     constructor(nombre, precio, stock) {
         this.id = Date.now().toString(36);
@@ -213,14 +116,10 @@ const mostrarPrendas = (prendas) => {
         tarjetaPrendas.appendChild(btnEditar);
 
         // Eliminar prenda
-
         let btnEliminar = document.createElement("button");
         btnEliminar.classList.add("btn", "btn-danger");
         btnEliminar.innerHTML = "Eliminar";
         tarjetaPrendas.appendChild(btnEliminar);
-
-
-
 
         btnEliminar.onclick = () => {
             eliminarPrenda(index);
@@ -228,10 +127,6 @@ const mostrarPrendas = (prendas) => {
         }
 
         btnEditar.onclick = () => editarPrenda(index);
-
-
-        // Local Storage
-
 
     })    
 
@@ -241,7 +136,6 @@ const eliminarPrenda = (index) => {
     prendas.splice(index, 1);
     mostrarPrendas(prendas);
 };
-
 
 // funcion para editar prenda
 
@@ -267,18 +161,3 @@ buscar.oninput = (event) => {
         mostrarPrendas(prendasFiltradas);
     }
 }
-
-
-
-
-
-// localStorage.setItem("prendas", prendas);
-// localStorage.setItem("prendasStringify", JSON.stringify(prendas));
-
-// let prendasStringify = localStorage.getItem("prendasStringify");
-// console.log(prendasStringify);
-// console.log(typeof prendasStringify);
-
-// let prendasParseadas = JSON.parse(localStorage.getItem("prendasStringify"))
-
-
